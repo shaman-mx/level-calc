95d0d0bb6e825cb452a4d3ed1b412df13090b043
shaman-mx <shamanmx@gmail.com>
2025-09-07 14:12:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function calcBefore() {
+  let val = parseFloat(document.getElementById("beforeInput").value) || 0;
+  let res = val * 105 / 95;
+  document.getElementById("beforeResult").innerText = res.toFixed(2);
+}
+
+function calcAfter() {
+  let val = parseFloat(document.getElementById("afterInput").value) || 0;
+  let res = val * 110 / 95;
+  document.getElementById("afterResult").innerText = res.toFixed(2);
+}
+
 function toggleClear(input) {
   const btn = input.parentElement.querySelector('.clear-btn');
   btn.style.display = input.value ? 'block' : 'none';
@@ -6,5 +18,7 @@ function toggleClear(input) {
 function clearInput(id) {
   const input = document.getElementById(id);
   input.value = '';
+  if (id === "beforeInput") calcBefore();
+  if (id === "afterInput") calcAfter();
   toggleClear(input);
 }
