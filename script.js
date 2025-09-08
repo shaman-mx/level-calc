0dc88a7485eb8c2a548e9397395f0c678aaff658
shaman-mx <shamanmx@gmail.com>
2025-09-08 16:51:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -253,24 +253,6 @@ function startProgress() {
   updateInfoUI();
 
   timer = setInterval(tick, 200);         // tick mượt 5 lần/giây
-}
-function startProgress() {
-  clearInterval(timer);
-  if (!levelSelect) return;
-
-  const key = levelSelect.value;          // đã gộp Level + Giai đoạn
-  if (!crystalData[key]) return;
-
-  totalExp = crystalData[key].exp;
-  baseRate = crystalData[key].rate;
-
-  // Reset khi đổi Level hoặc bấm reset
-  accExp   = 0;
-  lastTick = Date.now();
-  updateProgressUI();
-  updateInfoUI();
-
-  timer = setInterval(tick, 200);         // tick mượt 5 lần/giây
 
 } // <-- Kết thúc startProgress() ở đây
 
