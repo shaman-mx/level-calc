4f1cd9a176a8d2c9532e20f8b5b6473a37e6727d
shaman-mx <shamanmx@gmail.com>
2025-09-28 22:42:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -126,8 +126,8 @@
     else if (cd >= 501) buffPercent += 0.05;
     else if (cd >= 200) buffPercent += 0.03;
 
-    const keBang = parseInt(keBangTam?.value || 0, 10);
-    const extra = keBang > 0 ? Math.ceil(keBang / 2) : 0;
+	const keBang = parseInt(keBangTam?.value || 0, 10);
+	const extra = keBang > 0 ? keBang : 0;
 
     const hmc = parseInt(huyenMinhCong?.value || 0, 10);
     buffPercent += hmc * 0.01;
